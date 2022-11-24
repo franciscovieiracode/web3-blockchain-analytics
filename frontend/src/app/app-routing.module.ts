056a0f98dc7d737af44173bd0da4223b55ad2ecd
Pedro Teixeira <8150206@estg.ipp.pt>
2022-11-24 17:12:21 +0000

    login commit

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/auth/login/login.component';
 import { RegisterComponent } from './components/auth/register/register.component';
 import { HomePageComponent } from './components/landing-page/home-page/home-page.component';
 
 const routes: Routes = [
   {path:'', component: HomePageComponent},
+  {path:'login', component: LoginComponent},
   {path:'register', component: RegisterComponent},
   {path:'**', redirectTo: ''}
 ];

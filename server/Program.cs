33c890bae00fc6021a981d97e238186547428a40
FRANCISCO JOAO MOREIRA VIEIRA <8170464@estg.ipp.pt>
2023-01-11 15:17:58 +0000

    Revert "Merge branch 'refactor-structure' into 'master'"
    This reverts merge request !43

--- server/Program.cs
+++ server/Program.cs
@@ -15,6 +15,8 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<LDSDBContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionString")));
 
+builder.Services.AddSwaggerGen();
+
 builder.Services.Configure<JwtConfig>(builder.Configuration.GetSection("JwtConfig"));
 
 builder.Services.Configure<IdentityOptions>(options =>
@@ -61,8 +63,20 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
 
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
+app.UseSwagger(options =>
+{
+    options.SerializeAsV2 = true;
+});
+
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+});
 
 app.UseHttpsRedirection();
 
@@ -76,6 +90,6 @@ app.UseAuthorization();
 app.UseCors(x => x.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Auth2}/{action=Index}/{id?}");
+    pattern: "{controller=Auth}/{action=Index}/{id?}");
 
 app.Run();
